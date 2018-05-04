04aea92695ca308e7e74a71153508684a4bf7190
opensunil <opensunil@gmail.com>
2018-05-04 17:02:33 -0400

    added ansible install step

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 echo Start setup
 git config --global user.email "opensunil@gmail.com"
 git config --global user.name "Sunil Varghese"
+
+echo Installing ansible/xenial-backports.  Enter password:
+sudo apt-get --assume-yes install ansible/xenial-backport
