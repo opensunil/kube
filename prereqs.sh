2c3db8f1744e7b939b8c765912ae69401f4827e2
opensunil <opensunil@gmail.com>
2018-05-07 02:43:29 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -3,6 +3,7 @@
 VARIABLE_SCRIPT=variables.sh
 source $VARIABLE_SCRIPT
 
+echo "Installing ansible. Enter password if needed."
 sudo apt -y install ansible
 
 echo "Create $KUBE_USER user if it does not exist.  Provide password if asked."
