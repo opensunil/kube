0bf9b7efea49ee7ab62d6ec23c39b91202c6130f
opensunil <opensunil@gmail.com>
2018-05-07 00:59:55 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -3,9 +3,9 @@
 VARIABLE_SCRIPT=variables.sh
 source $VARIABLE_SCRIPT
 
-echo "Create kube user if it does not exist.  Provide password if asked."
+echo "Create $USER user if it does not exist.  Provide password if asked."
 id -u $USER &>/dev/null || sudo adduser kube --gecos "First Last,RoomNumber,WorkPhone,HomePhone"
 
-echo "Switch to kube user.  Enter rock64 password if asked."
+echo "Switch to $USER user.  Enter rock64 password if asked."
 #sudo su - kube
 #sudo -u $USER "$REPO_FOLDER/$STARTUP_SCRIPT"
