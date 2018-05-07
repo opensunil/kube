46fea8c0d9ac3e0c208ab4551820e0d6af3f0c41
opensunil <opensunil@gmail.com>
2018-05-07 01:58:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ VARIABLE_SCRIPT=variables.sh
 source $VARIABLE_SCRIPT
 
 #whoami
+echo Current running user
 whoami
+echo $USER
 
 # reset changes
 if [ -d "$REPO_FOLDER" ]; then
@@ -20,6 +22,11 @@ cd $REPO_FOLER
 echo Git user setup
 git config user.email "opensunil@gmail.com"
 git config user.name "Sunil Varghese"
+
+#whoami
+echo Current running user
+whoami
+echo $USER
 
 echo Installing ansible/xenial-backports.  Enter password:
 #sudo apt-get --assume-yes install ansible/xenial-backport
