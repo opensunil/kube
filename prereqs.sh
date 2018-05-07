9a3b18a44e418c29188c0a538252d2cd6a167fe5
opensunil <opensunil@gmail.com>
2018-05-07 00:40:14 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -10,21 +10,5 @@ echo "Create kube user if it does not exist.  Provide password if asked."
 id -u $USER &>/dev/null || sudo adduser kube --gecos "First Last,RoomNumber,WorkPhone,HomePhone"
 
 echo "Switch to kube user.  Enter rock64 password if asked."
-sudo su - kube
-
-#whoami
-whoami
-
-# reset changes
-if [ -d "$REPO_FOLDER" ]; then
-	rm -rf $REPO_FOLDER;
-fi
-
-# Clone repo 
-git clone $REPO $REPO_FOLDER
-
-# Run repo setup file
-if [ -f "$REPO_FOLDER/$STARTUP_SCRIPT" ]; then
-	chmod 744 "$REPO_FOLDER/$STARTUP_SCRIPT"
-	source  "$REPO_FOLDER/$STARTUP_SCRIPT"
-fi 
+#sudo su - kube
+sudo -u $USER 'source "$REPO_FOLDER/$STARTUP_SCRIPT"'
