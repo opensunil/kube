35de3a88b733300a2a1ea1cab89e5fe6ce589d42
opensunil <opensunil@gmail.com>
2018-05-07 12:17:30 -0400

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Setup variables
-KUBE_USER=kube
+# Peer server information
+export KUBE_USER=kube
+export KUBE_HOST=kube.onthewifi.com
+export KPORT1=2122
+export KPORT2=2222
+export KPORT3=2322
+
+# Filesystem variables
 STARTUP_SCRIPT=setup.sh
 REPO_FOLDER=/tmp/kube
 
