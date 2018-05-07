a302afe6fd91cf7ecba79dd3557cd2faa1fc5ec4
opensunil <opensunil@gmail.com>
2018-05-07 02:12:33 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ -d "kube" ]; then
-	cp ./kube/variables.sh .
-fi
+#if [ -d "kube" ]; then
+#	cp ./kube/variables.sh .
+#fi
 
 VARIABLE_SCRIPT=variables.sh
 source $VARIABLE_SCRIPT
 
-if [ -d "$REPO_FOLDER" ]; then
-	cp $REPO_FOLDER/$STARTUP_SCRIPT .
-fi
+#if [ -d "$REPO_FOLDER" ]; then
+#	cp $REPO_FOLDER/$STARTUP_SCRIPT .
+#fi
 
 echo "Create $KUBE_USER user if it does not exist.  Provide password if asked."
 id -u $KUBE_USER &>/dev/null || sudo adduser $KUBE_USER --gecos "First Last,RoomNumber,WorkPhone,HomePhone"; sudo usermod -aG sudo $KUBE_USER
