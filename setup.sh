76187e406f9e73f72b98241ad26acb54aad36339
opensunil <opensunil@gmail.com>
2018-05-07 00:41:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-echo Start setup
+#whoami
+whoami
+
+# reset changes
+if [ -d "$REPO_FOLDER" ]; then
+	rm -rf $REPO_FOLDER;
+fi
+
+# Clone repo 
+git clone $REPO $REPO_FOLDER
+
+# Run repo setup file
+if [ -f "$REPO_FOLDER/$STARTUP_SCRIPT" ]; then
+	source  "$REPO_FOLDER/$STARTUP_SCRIPT"
+fi 
+
+
+echo Git user setup
 git config --global user.email "opensunil@gmail.com"
 git config --global user.name "Sunil Varghese"
 
