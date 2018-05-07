6f1722537d8ee0456ac71157475555c521d6b8a5
opensunil <opensunil@gmail.com>
2018-05-07 01:24:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ source $VARIABLE_SCRIPT
 whoami
 
 # reset changes
-#if [ -d "$REPO_FOLDER" ]; then
-#	rm -rf $REPO_FOLDER;
-#fi
+if [ -d "$REPO_FOLDER" ]; then
+	rm -rf $REPO_FOLDER;
+fi
 
 # Clone repo 
-#git clone $REPO $REPO_FOLDER
+git clone $REPO $REPO_FOLDER
 
 echo Git user setup
 git config --global user.email "opensunil@gmail.com"
