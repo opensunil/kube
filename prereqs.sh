db8a734ad364829bec57e5b8f0ac9acd187e54e0
opensunil <opensunil@gmail.com>
2018-05-07 00:11:43 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Setup variables
+USER=kube
 REPO=https://github.com/opensunil/kube.git
 REPO_FOLDER=/tmp/kube
 STARTUP_SCRIPT=setup.sh
+
+echo "Create kube user if it does not exist.  Provide password if asked."
+id -u $USER &>/dev/null || sudo adduser kube --gecos "First Last,RoomNumber,WorkPhone,HomePhone"
 
 # reset changes
 if [ -d "$REPO_FOLDER" ]; then
