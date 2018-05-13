623c982277ffed1b4622d5d6148d0686c8bdcb3e
opensunil <opensunil@gmail.com>
2018-05-12 21:50:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,20 +8,28 @@ source $VARIABLE_SCRIPT
 echo Current user: $USER
 
 # reset changes
-if [ -d "$REPO_FOLDER" ]; then
-	sudo rm -rf $REPO_FOLDER;
+#if [ -d "$REPO_FOLDER" ]; then
+#	sudo rm -rf $REPO_FOLDER;
+#fi
+
+if [ ! -d "$REPO_FOLDER" ]; then
+	# Clone repo 
+	sudo git clone $REPO $REPO_FOLDER
+	sudo chown -R $USER $REPO_FOLDER
+
+	cd $REPO_FOLDER
+	echo Current folder
+	pwd
+
+	echo Git user setup
+	git config user.email "opensunil@gmail.com"
+	git config user.name "Sunil Varghese"
+
+	#whoami
+	echo Current user: $USER
+else
+	cd $REPO_FOLDER
+	git pull
 fi
 
-# Clone repo 
-git clone $REPO $REPO_FOLDER
 
-cd $REPO_FOLDER
-echo Current folder
-pwd
-
-echo Git user setup
-git config user.email "opensunil@gmail.com"
-git config user.name "Sunil Varghese"
-
-#whoami
-echo Current user: $USER
