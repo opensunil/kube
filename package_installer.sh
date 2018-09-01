e7fa99e90473f6a514f3cb6b2a797858ae588ae4
opensunil <opensunil@gmail.com>
2018-09-01 11:52:06 -0400

    Update package_installer.sh

--- package_installer.sh
+++ package_installer.sh
@@ -2,5 +2,6 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $1|grep "install ok installed"
 echo Checking for somelib: $PKG_OK
 if [ "" == "$PKG_OK" ]; then
   echo "$1 not installed. Setting up $1."
-  # sudo apt-get --force-yes --yes install $1
+  sudo apt-get --allow-downgrades --allow-remove-essential --allow-change-held-packages install $1
+  # DEPRECATED sudo apt-get --force-yes --yes install $1
 fi
