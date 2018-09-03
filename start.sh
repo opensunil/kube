f661d423b6974b2425c9c5956ee4d8c7a9a06683
opensunil <opensunil@gmail.com>
2018-09-03 15:18:14 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,11 @@ cd $REPO_HOST_FOLDER
 git clone https://github.com/opensunil/kube.git
 rm *.sh
 cp kube/*.sh .
+
 ./prereqs.sh
+
+echo "Switch to $KUBE_USER user.  Enter rock64 password if asked."
+#sudo su - kube
+sudo -u $KUBE_USER "./$SETUP_SCRIPT"
+
 cd -
