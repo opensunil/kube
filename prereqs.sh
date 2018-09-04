43b85c62c1859d76c2d624813d1317d8ae562152
opensunil <opensunil@gmail.com>
2018-09-03 20:08:36 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -12,9 +12,8 @@ sudo apt install -y apt-transport-https
 echo Install docker.io
 sudo apt install -y docker.io
 
-echo "Installing ansible. Enter password if needed."
-#sudo apt -y install ansible
-./package_installer.sh ansible
+#echo "Installing ansible. Enter password if needed."
+#./package_installer.sh ansible
 
-echo "Create $KUBE_USER user if it does not exist.  Provide password if asked."
-id -u $KUBE_USER &>/dev/null || sudo adduser $KUBE_USER --gecos "First Last,RoomNumber,WorkPhone,HomePhone"; sudo usermod -aG sudo $KUBE_USER
+#echo "Create $KUBE_USER user if it does not exist.  Provide password if asked."
+#id -u $KUBE_USER &>/dev/null || sudo adduser $KUBE_USER --gecos "First Last,RoomNumber,WorkPhone,HomePhone"; sudo usermod -aG sudo $KUBE_USER
